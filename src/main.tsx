8e77d942527af7a9c2a3562c193f37930e6618fc
Taylor Faragher <faragher6@gmail.com>
2024-05-03 16:06:49 -0500

    Adds ErrorBoundary to app for catching errors

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './App';
 import {BrowserRouter} from 'react-router-dom';
+import {ErrorBoundary} from 'react-error-boundary';
+import {ErrorPage} from './pages';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <ErrorBoundary FallbackComponent={ErrorPage}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ErrorBoundary>
     </React.StrictMode>
 );
