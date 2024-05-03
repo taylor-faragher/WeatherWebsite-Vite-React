b9ed3ea311129bf75e09be678039d3bb6a76c1d4
Taylor Faragher <faragher6@gmail.com>
2024-05-03 17:32:56 -0500

    Minor code updates

--- src/router.tsx
+++ src/router.tsx
@@ -4,9 +4,10 @@ import {ErrorPage, HomePage, ResultPage} from './pages';
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path={'/error'} element={<ErrorPage />} />
-            <Route path={'/main'} element={<HomePage />} />
-            <Route path={'/result'} element={<ResultPage />} />
+            <Route path='/' element={<HomePage />} />
+            <Route path='main' element={<HomePage />} />
+            <Route path='result' element={<ResultPage />} />
+            <Route path='error' element={<ErrorPage />} />
             <Route path='*' element={<HomePage />} />
         </Routes>
     );
