2c995cd2e56adb45981fba955a91c86b2596d911
Taylor Faragher <faragher6@gmail.com>
2024-05-12 19:27:05 -0500

    Adds null check to object mapper

--- src/mappers/zipCodeDataMapper.ts
+++ src/mappers/zipCodeDataMapper.ts
@@ -11,8 +11,8 @@ export const mapZipCodeData = (data: WeatherResults): WeatherItem | undefined =>
     const description = pascalize(data.weather[0].description);
 
     return {
-        currentTemp: data?.main.temp,
-        windSpeed: data?.wind.speed as number,
+        currentTemp: data?.main?.temp,
+        windSpeed: data?.wind?.speed as number,
         description: description,
         majorCity: data.name as string,
         image: weatherPic,
