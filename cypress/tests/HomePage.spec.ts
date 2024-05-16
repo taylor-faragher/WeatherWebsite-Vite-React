612e18aba74da9c67a54635d9e615b336a818fe9
Taylor Faragher <faragher6@gmail.com>
2024-05-16 10:18:41 -0500

    Adds new e2e tests for updated UI

--- cypress/tests/HomePage.spec.ts
+++ cypress/tests/HomePage.spec.ts
@@ -2,54 +2,53 @@ describe('HomePage E2E Tests', () => {
     it(`All Page elements should be visible`, () => {
         cy.tid(`HomePage_WelcomeTitle`).should('be.visible');
         cy.tid(`HomePage_Title`).should('be.visible');
-        cy.tid(`HomePage_ZipCodeForm`).should('be.visible');
-        cy.tid(`HomePage_ZipCodeButtonSearch`).should('be.visible');
-        cy.tid(`HomePage_ErrorMessage`).should('not.exist');
+        cy.tid(`ZipCodeForm`).should('be.visible');
+        cy.tid(`ZipCodeButtonSearch`).should('be.visible');
+        cy.tid(`ErrorMessage`).should('not.exist');
     });
 
     it(`Should navigate to results page when valid zip code is entered`, () => {
-        cy.tid(`HomePage_ZipCodeForm`).should('be.visible');
-        cy.tid(`HomePage_ErrorMessage`).should('not.exist');
-        cy.tid(`HomePage_ZipCodeInput`).type('20020', {delay: 100});
-        cy.tid(`HomePage_ZipCodeInput`).trigger('event');
-        cy.tid(`HomePage_ZipCodeForm`).submit();
+        cy.tid(`ZipCodeForm`).should('be.visible');
+        cy.tid(`ErrorMessage`).should('not.exist');
+        cy.tid(`ZipCodeInput`).type('20020', {delay: 100});
+        cy.tid(`ZipCodeForm`).submit();
         cy.url().should('contain', '/result?zipCode=20020');
     });
 
     it(`Should navigate to results page when valid zip code is entered and user types Enter`, () => {
-        cy.tid(`HomePage_ZipCodeForm`).should('be.visible');
-        cy.tid(`HomePage_ErrorMessage`).should('not.exist');
-        cy.tid(`HomePage_ZipCodeInput`).should('be.visible').type('20020{enter}');
+        cy.tid(`ZipCodeForm`).should('be.visible');
+        cy.tid(`ErrorMessage`).should('not.exist');
+        cy.tid(`ZipCodeInput`).should('be.visible').type('20020{enter}');
         cy.url().should('contain', '/result?zipCode=20020');
     });
 
     it(`Should show Error message when ZipCode is too few characters`, () => {
-        cy.tid(`HomePage_ZipCodeForm`).should('be.visible');
-        cy.tid(`HomePage_ZipCodeInput`).should('be.visible').type('111');
-        cy.tid(`HomePage_ZipCodeButtonSearch`).should('be.visible').click();
-        cy.tid(`HomePage_ErrorMessage`).should('be.visible');
+        cy.tid(`ZipCodeForm`).should('be.visible');
+        cy.tid(`ZipCodeInput`).should('be.visible').type('111');
+        cy.tid(`ZipCodeButtonSearch`).should('be.visible').click();
+        cy.tid(`ErrorMessage`).should('be.visible');
     });
 
     it(`Should show Error message when ZipCode is blank`, () => {
-        cy.tid(`HomePage_ZipCodeForm`).should('be.visible');
-        cy.tid(`HomePage_ZipCodeInput`).should('be.visible');
-        cy.tid(`HomePage_ZipCodeButtonSearch`).should('be.visible').click();
-        cy.tid(`HomePage_ErrorMessage`).should('be.visible');
+        cy.tid(`ZipCodeForm`).should('be.visible');
+        cy.tid(`ZipCodeInput`).should('be.visible');
+        cy.tid(`ZipCodeButtonSearch`).should('be.visible').click();
+        cy.tid(`ErrorMessage`).should('be.visible');
     });
 
     it(`Should only allow numbers to be entered in zip code search bar`, () => {
-        cy.tid(`HomePage_ZipCodeForm`).should('be.visible');
-        cy.tid(`HomePage_ZipCodeInput`).should('be.visible');
-        cy.tid(`HomePage_ZipCodeInput`).type('abcd200ljsdf20');
-        cy.tid(`HomePage_ZipCodeInput`).should('have.value', '20020');
+        cy.tid(`ZipCodeForm`).should('be.visible');
+        cy.tid(`ZipCodeInput`).should('be.visible');
+        cy.tid(`ZipCodeInput`).type('abcd200ljsdf20');
+        cy.tid(`ZipCodeInput`).should('have.value', '20020');
     });
 
     it(`Should navigate to results page after valid zip code is entered and error message is showing`, () => {
-        cy.tid(`HomePage_ZipCodeForm`).should('be.visible');
-        cy.tid(`HomePage_ZipCodeInput`).should('be.visible').type('200');
-        cy.tid(`HomePage_ZipCodeButtonSearch`).should('be.visible').click();
-        cy.tid(`HomePage_ErrorMessage`).should('be.visible');
-        cy.tid(`HomePage_ZipCodeInput`).should('be.visible').type('20020{enter}');
+        cy.tid(`ZipCodeForm`).should('be.visible');
+        cy.tid(`ZipCodeInput`).should('be.visible').type('200');
+        cy.tid(`ZipCodeButtonSearch`).should('be.visible').click();
+        cy.tid(`ErrorMessage`).should('be.visible');
+        cy.tid(`ZipCodeInput`).should('be.visible').type('20020{enter}');
         cy.url().should('contain', '/result?zipCode=20020');
     });
 });
--- cypress/tests/ResultPage.spec.ts
+++ cypress/tests/ResultPage.spec.ts
@@ -1,13 +1,29 @@
 describe('ResultPage E2E Tests', () => {
     it(`All Page elements should be visible and clicking return home navigates back to home page`, () => {
         cy.visit('/result?zipCode=20020');
-        cy.wait('@getWeather');
+        cy.wait('@getWeather1');
         cy.tid(`ResultPage_ZipCodeTitle`).should('be.visible');
         cy.tid(`ResultPage_WeatherPic`).should('be.visible');
         cy.tid(`ResultPage_CurrentTemp`).should('be.visible');
         cy.tid(`ResultPage_Description`).should('be.visible');
+        cy.tid(`ZipCodeForm`).should('be.visible');
+        cy.tid(`ZipCodeInput`).should('be.visible');
+        cy.tid(`ZipCodeButtonSearch`).should('be.visible');
         cy.tid('ReturnButton').should('be.visible').click();
         cy.url().should('contain', '/main');
+    });
+
+    it(`User can search zip Code again from result page`, () => {
+        cy.visit('/result?zipCode=20020');
+        cy.wait('@getWeather1');
+        cy.tid(`ResultPage_CurrentTemp`).should('be.visible');
+        cy.tid(`ZipCodeForm`).should('be.visible');
+        cy.tid(`ZipCodeInput`).should('be.visible');
+        cy.tid(`ZipCodeInput`).type('20005', {delay: 100});
+        cy.tid(`ZipCodeForm`).submit();
+        cy.url().should('contain', '/result?zipCode=20005');
+        cy.wait('@getWeather2');
+        cy.tid(`ResultPage_CurrentTemp`).should('be.visible');
     });
 
     it(`Passing partial zip code in parameters causes a redirect to Error page`, () => {
@@ -28,5 +44,26 @@ describe('ResultPage E2E Tests', () => {
     it(`Should navigate to error page if no parameters are passed to result page`, () => {
         cy.visit('/result');
         cy.url().should('contain', '/error');
+    });
+
+    it(`Should show Error message when ZipCode is too few characters`, () => {
+        cy.tid(`ZipCodeForm`).should('be.visible');
+        cy.tid(`ZipCodeInput`).should('be.visible').type('111');
+        cy.tid(`ZipCodeButtonSearch`).should('be.visible').click();
+        cy.tid(`ErrorMessage`).should('be.visible');
+    });
+
+    it(`Should show Error message when ZipCode is blank`, () => {
+        cy.tid(`ZipCodeForm`).should('be.visible');
+        cy.tid(`ZipCodeInput`).should('be.visible');
+        cy.tid(`ZipCodeButtonSearch`).should('be.visible').click();
+        cy.tid(`ErrorMessage`).should('be.visible');
+    });
+
+    it(`Should only allow numbers to be entered in zip code search bar`, () => {
+        cy.tid(`ZipCodeForm`).should('be.visible');
+        cy.tid(`ZipCodeInput`).should('be.visible');
+        cy.tid(`ZipCodeInput`).type('abcd200ljsdf20');
+        cy.tid(`ZipCodeInput`).should('have.value', '20020');
     });
 });
