0e3210a6de3c9ae62f814dd1ca077a7c2bc46a28
Taylor Faragher <faragher6@gmail.com>
2024-05-23 13:01:25 -0500

    Initial creation of header and header icons

---
+++ src/pages/shared/Header.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import {FaHouse, FaCircleQuestion, FaGithub} from 'react-icons/fa6';
+import {HeaderIcon} from './HeaderIcon';
+
+const HeaderWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid #9b9b9b;
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 75px;
+    background: #202225;
+    z-index: 9;
+`;
+
+const ButtonWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    width: 100%;
+    max-width: 500px;
+`;
+
+export const Header = () => {
+    const showHeader = location.pathname.toLowerCase() !== '/error'; //don't show for error page
+    return (
+        <>
+            {showHeader && (
+                <HeaderWrapper>
+                    <ButtonWrapper>
+                        <HeaderIcon icon={<FaHouse size='25' />} text='Home' />
+                        <HeaderIcon icon={<FaCircleQuestion size='25' />} text='FAQ' />
+                        <HeaderIcon icon={<FaGithub size='25' />} text='See the Code for this Site!' />
+                    </ButtonWrapper>
+                </HeaderWrapper>
+            )}
+        </>
+    );
+};
---
+++ src/pages/shared/HeaderIcon.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+const HeaderIconWrapper = styled.div`
+    display: flex;
+    position: relative;
+    align-items: center;
+    justify-content: center;
+    height: 3rem;
+    width: 3rem;
+    cursor: pointer;
+    background-color: rgb(156 163 175);
+    margin-right: auto;
+    margin-left: auto;
+    background-color: grey;
+    border-radius: 1.5rem;
+    transition-property: all;
+    transition-duration: 300ms;
+    color: rgb(34 197 94);
+    &:hover {
+        border-radius: 0.5rem;
+        background-color: rgb(22 163 74);
+        color: rgb(255 255 255);
+    }
+`;
+
+const HeaderTooltip = styled.span`
+    position: absolute;
+    width: auto;
+    margin: 0.5rem;
+    border-radius: 0.375rem;
+    min-width: max-content;
+    top: 50px;
+    padding: 0.5rem;
+    margin: 0.5rem;
+    background-color: rgb(75 85 99);
+    border-radius: 0.375rem;
+    box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+    transition-property: all;
+    transition-duration: 100ms;
+    transform: scale(0);
+    transform-origin: top;
+
+    ${HeaderIconWrapper}:hover & {
+        transform: scale(1);
+    }
+`;
+
+export const HeaderIcon = ({icon, text}) => (
+    <HeaderIconWrapper>
+        {icon}
+        <HeaderTooltip>{text}</HeaderTooltip>
+    </HeaderIconWrapper>
+);
