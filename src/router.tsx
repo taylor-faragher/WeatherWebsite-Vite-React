8f54f41b60ea9703ba3c417137a816951cf38d52
Taylor Faragher <faragher6@gmail.com>
2024-05-23 13:02:05 -0500

    Adds Header component to router to display on pages

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,19 @@
 import {Routes, Route} from 'react-router-dom';
 import {ErrorPage, HomePage, ResultPage} from './pages';
+import {Header} from './pages/shared/Header';
 
 const AppRoutes = () => {
     return (
-        <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='main' element={<HomePage />} />
-            <Route path='result' element={<ResultPage />} />
-            <Route path='error' element={<ErrorPage />} />
-            <Route path='*' element={<HomePage />} />
-        </Routes>
+        <>
+            <Header></Header>
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='main' element={<HomePage />} />
+                <Route path='result' element={<ResultPage />} />
+                <Route path='error' element={<ErrorPage />} />
+                <Route path='*' element={<HomePage />} />
+            </Routes>
+        </>
     );
 };
 
