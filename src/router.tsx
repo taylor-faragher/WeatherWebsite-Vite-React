57562c2b4c23e3c87d44cce535cd57e1454fcdee
Taylor Faragher <faragher6@gmail.com>
2024-05-24 08:02:55 -0500

    Import CSS; Add width to page parent component

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,16 @@
 import {Routes, Route} from 'react-router-dom';
 import {ErrorPage, HomePage, ResultPage} from './pages';
 import {Header} from './pages/shared/Header';
+import './AppRoutes.css';
+import {styled} from 'styled-components';
+
+const AppWrapper = styled.div`
+    width: 100vw;
+`;
 
 const AppRoutes = () => {
     return (
-        <>
+        <AppWrapper data-test-id='AppWrapper'>
             <Header></Header>
             <Routes>
                 <Route path='/' element={<HomePage />} />
@@ -13,7 +19,7 @@ const AppRoutes = () => {
                 <Route path='error' element={<ErrorPage />} />
                 <Route path='*' element={<HomePage />} />
             </Routes>
-        </>
+        </AppWrapper>
     );
 };
 
