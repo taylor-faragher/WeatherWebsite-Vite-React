2fee83933a9d2f51854fd6ca556d8f7bcd5dc866
Taylor Faragher <faragher6@gmail.com>
2024-05-28 18:52:51 -0500

    Adds Theme file with various theme values

---
+++ src/Theme.ts
@@ -0,0 +1,23 @@
+export const lightTheme = {
+    body: '#FFF',
+    headerBody: '#FFF',
+    text: '#363537',
+    toggleBorder: '#363537',
+    background: '#363537',
+    headerIconColor: '#363537',
+    headerIconBackgroundColor: 'white',
+    borderBottom: '2px solid',
+    zipCodeInputColor: 'black',
+};
+
+export const darkTheme = {
+    body: '#363537',
+    headerBody: 'black',
+    text: '#FAFAFA',
+    toggleBorder: '#6B8096',
+    background: '#999',
+    headerIconColor: 'rgb(34 197 94)',
+    headerIconBackgroundColor: 'grey',
+    borderBottom: 'revert',
+    zipCodeInputColor: 'white',
+};
