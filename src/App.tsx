5b08784ed38ee6c55d9dd06281ca3b826cb0aee7
Taylor Faragher <faragher6@gmail.com>
2024-05-31 11:00:11 -0500

    Moves styling and other wrappers out of routers so that router can just route things

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import styled, {ThemeProvider} from 'styled-components';
+import AppRoutes from './router';
+import {GlobalStyles} from './GlobalStyles';
+import {Account} from './services/Account';
+import Status from './pages/status/Status';
+import {darkTheme, lightTheme} from './Theme';
+import {Header} from './pages/shared/Header';
+import {useDarkMode} from './hooks/useDarkMode';
+
+const AppWrapper = styled.div`
+    width: 100vw;
+`;
+
+export const App = () => {
+    const [theme, themeToggler, mountedComponent] = useDarkMode();
+    const themeMode = theme === 'light' ? lightTheme : darkTheme;
+    if (!mountedComponent) return <div />;
+    return (
+        <ThemeProvider theme={themeMode}>
+            <GlobalStyles />
+            <Account>
+                <Status />
+                <AppWrapper data-test-id='AppWrapper'>
+                    <Header theme={theme} themeSwitch={themeToggler}></Header>
+                    <AppRoutes />
+                </AppWrapper>
+            </Account>
+        </ThemeProvider>
+    );
+};
+
+export default App;
--- src/router.tsx
+++ src/router.tsx
@@ -1,44 +1,20 @@
 import {Routes, Route, Navigate} from 'react-router-dom';
 import {ErrorPage, HomePage, ResultPage, SignUpPage} from './pages';
-import {Header} from './pages/shared/Header';
 import './AppRoutes.css';
-import {ThemeProvider, styled} from 'styled-components';
-import {GlobalStyles} from './GlobalStyles';
-import {darkTheme, lightTheme} from './Theme';
-import {useDarkMode} from './hooks/useDarkMode';
 import LoginPage from './pages/login/LoginPage';
-import {Account} from './services/Account';
-import Status from './pages/status/Status';
-
-const AppWrapper = styled.div`
-    width: 100vw;
-`;
 
 const AppRoutes = () => {
-    const [theme, themeToggler, mountedComponent] = useDarkMode();
-    const themeMode = theme === 'light' ? lightTheme : darkTheme;
-
-    if (!mountedComponent) return <div />;
     return (
-        <ThemeProvider theme={themeMode}>
-            <GlobalStyles />
-            <Account>
-                <Status />
-                <AppWrapper data-test-id='AppWrapper'>
-                    <Header theme={theme} themeSwitch={themeToggler}></Header>
-                    <Routes>
-                        <Route path='/' element={<HomePage />} />
-                        <Route path='main' element={<HomePage />} />
-                        <Route path='result' element={<ResultPage />} />
-                        <Route path='info' element={<Navigate to='/main' replace />} />
-                        <Route path='signup' element={<SignUpPage />} />
-                        <Route path='login' element={<LoginPage />} />
-                        <Route path='error' element={<ErrorPage />} />
-                        <Route path='*' element={<HomePage />} />
-                    </Routes>
-                </AppWrapper>
-            </Account>
-        </ThemeProvider>
+        <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='main' element={<HomePage />} />
+            <Route path='result' element={<ResultPage />} />
+            <Route path='info' element={<Navigate to='/main' replace />} />
+            <Route path='signup' element={<SignUpPage />} />
+            <Route path='login' element={<LoginPage />} />
+            <Route path='error' element={<ErrorPage />} />
+            <Route path='*' element={<HomePage />} />
+        </Routes>
     );
 };
 
