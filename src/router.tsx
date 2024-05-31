f12a9ff9cde0703fb17ce8dc6eb531f699d0baa0
Taylor Faragher <faragher6@gmail.com>
2024-05-31 17:59:37 -0500

    Updates Router table to use Private Route for profile

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,12 @@
 import {Routes, Route, Navigate} from 'react-router-dom';
-import {ErrorPage, HomePage, ResultPage, SignUpPage} from './pages';
+import {ErrorPage, HomePage, ResultPage, SignUpPage, ProfilePage} from './pages';
 import './AppRoutes.css';
 import LoginPage from './pages/login/LoginPage';
+import PrivateRoute from './PrivateRoute';
+import useLoginStatus from './hooks/useLoginStatus';
 
 const AppRoutes = () => {
+    const status = useLoginStatus();
     return (
         <Routes>
             <Route path='/' element={<HomePage />} />
@@ -12,6 +15,14 @@ const AppRoutes = () => {
             <Route path='info' element={<Navigate to='/main' replace />} />
             <Route path='signup' element={<SignUpPage />} />
             <Route path='login' element={<LoginPage />} />
+            <Route
+                path='profile'
+                element={
+                    <PrivateRoute isAuthenticated={status}>
+                        <ProfilePage />
+                    </PrivateRoute>
+                }
+            />
             <Route path='error' element={<ErrorPage />} />
             <Route path='*' element={<HomePage />} />
         </Routes>
