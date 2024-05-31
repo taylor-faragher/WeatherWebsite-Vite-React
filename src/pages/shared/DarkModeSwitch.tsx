475afb6e00b7231e07ae47b53eabb84760e04f83
Taylor Faragher <faragher6@gmail.com>
2024-05-31 16:53:36 -0500

    Removes margin from dark mode switch to help with spacing

--- src/pages/shared/DarkModeSwitch.tsx
+++ src/pages/shared/DarkModeSwitch.tsx
@@ -13,8 +13,6 @@ const DarkModeButtonWrapper = styled.div`
     height: 3rem;
     width: 3rem;
     cursor: pointer;
-    margin-right: auto;
-    margin-left: auto;
     border-radius: 2rem;
     transition-property: all;
     transition-duration: 300ms;
