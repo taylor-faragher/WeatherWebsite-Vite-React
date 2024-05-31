10f6e8e54f9abea1aef35bb4a4559822eef6254b
Taylor Faragher <faragher6@gmail.com>
2024-05-31 10:05:00 -0500

    Adds signup and login paths

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,14 @@
 import {Routes, Route, Navigate} from 'react-router-dom';
-import {ErrorPage, HomePage, ResultPage} from './pages';
+import {ErrorPage, HomePage, ResultPage, SignUpPage} from './pages';
 import {Header} from './pages/shared/Header';
 import './AppRoutes.css';
 import {ThemeProvider, styled} from 'styled-components';
 import {GlobalStyles} from './GlobalStyles';
 import {darkTheme, lightTheme} from './Theme';
 import {useDarkMode} from './hooks/useDarkMode';
+import LoginPage from './pages/login/LoginPage';
+import {AccountPage} from './pages/account/AccountPage';
+import Status from './pages/status/Status';
 
 const AppWrapper = styled.div`
     width: 100vw;
@@ -19,17 +22,22 @@ const AppRoutes = () => {
     return (
         <ThemeProvider theme={themeMode}>
             <GlobalStyles />
-            <AppWrapper data-test-id='AppWrapper'>
-                <Header theme={theme} themeSwitch={themeToggler}></Header>
-                <Routes>
-                    <Route path='/' element={<HomePage />} />
-                    <Route path='main' element={<HomePage />} />
-                    <Route path='result' element={<ResultPage />} />
-                    <Route path='/info' element={<Navigate to='/main' replace />} />
-                    <Route path='error' element={<ErrorPage />} />
-                    <Route path='*' element={<HomePage />} />
-                </Routes>
-            </AppWrapper>
+            <AccountPage>
+                <Status />
+                <AppWrapper data-test-id='AppWrapper'>
+                    <Header theme={theme} themeSwitch={themeToggler}></Header>
+                    <Routes>
+                        <Route path='/' element={<HomePage />} />
+                        <Route path='main' element={<HomePage />} />
+                        <Route path='result' element={<ResultPage />} />
+                        <Route path='info' element={<Navigate to='/main' replace />} />
+                        <Route path='signup' element={<SignUpPage />} />
+                        <Route path='login' element={<LoginPage />} />
+                        <Route path='error' element={<ErrorPage />} />
+                        <Route path='*' element={<HomePage />} />
+                    </Routes>
+                </AppWrapper>
+            </AccountPage>
         </ThemeProvider>
     );
 };
