c498ddd791a487a36430632b5e5527cb0688c44e
Taylor Faragher <faragher6@gmail.com>
2024-05-31 17:58:47 -0500

    Updates login page to handle logout and redirect on login

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,8 +1,8 @@
 import {useContext, useState} from 'react';
 import {styled} from 'styled-components';
 import {AccountContext} from '../../services/Account';
-import Settings from '../shared/Settings';
-import Status from '../../hooks/useLoginStatus';
+import useLoginStatus from '../../hooks/useLoginStatus';
+import {useNavigate} from 'react-router-dom';
 
 const LoginPageWrapper = styled.div``;
 const LoginPageForm = styled.form``;
@@ -10,18 +10,22 @@ const LoginText = styled.h3``;
 const EmailInput = styled.input``;
 const PasswordInput = styled.input``;
 const SignUpButton = styled.button``;
+const LogoutButtonWrapper = styled.div``;
 
 const LoginPage = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const status = useLoginStatus();
+    const navigate = useNavigate();
 
-    const {authenticate} = useContext(AccountContext);
+    const {authenticate, logout} = useContext(AccountContext);
 
     const onSubmit = event => {
         event.preventDefault();
         authenticate(email, password)
             .then(data => {
                 console.log('Logged in!', data);
+                navigate('/main');
             })
             .catch(err => {
                 console.error('Failed to auth email and password: ', err);
@@ -30,15 +34,20 @@ const LoginPage = () => {
 
     return (
         <LoginPageWrapper>
-            <Status />
-            <Settings />
-            <LoginPageForm onSubmit={e => onSubmit(e)}>
-                <LoginText>Username</LoginText>
-                <EmailInput value={email} onChange={event => setEmail(event.target.value)}></EmailInput>
-                <LoginText>Password</LoginText>
-                <PasswordInput value={password} onChange={event => setPassword(event.target.value)}></PasswordInput>
-                <SignUpButton type='submit'>Login</SignUpButton>
-            </LoginPageForm>
+            {!status && (
+                <LoginPageForm onSubmit={e => onSubmit(e)}>
+                    <LoginText>Username</LoginText>
+                    <EmailInput value={email} onChange={event => setEmail(event.target.value)}></EmailInput>
+                    <LoginText>Password</LoginText>
+                    <PasswordInput value={password} onChange={event => setPassword(event.target.value)}></PasswordInput>
+                    <SignUpButton type='submit'>Login</SignUpButton>
+                </LoginPageForm>
+            )}
+            {status && (
+                <LogoutButtonWrapper>
+                    <button onClick={logout}>Logout</button>
+                </LogoutButtonWrapper>
+            )}
         </LoginPageWrapper>
     );
 };
