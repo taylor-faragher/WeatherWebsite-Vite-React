b3a8c10b079b50626fe3720fa6ec1855c53b1178
Taylor Faragher <faragher6@gmail.com>
2024-05-31 19:35:57 -0500

    Removes Private Route; no longer needed

--- src/PrivateRoute.tsx
+++
@@ -1,7 +0,0 @@
-import {Navigate} from 'react-router-dom';
-
-const PrivateRoute = ({children, isAuthenticated}) => {
-    return isAuthenticated ? children : <Navigate to='/login' />;
-};
-
-export default PrivateRoute;
