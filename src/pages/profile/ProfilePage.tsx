c3d4215ce112ed4dba10990753ddbac43aca25c6
Taylor Faragher <faragher6@gmail.com>
2024-06-01 13:33:51 -0500

    Adds styling and structure to profile page

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,12 +1,41 @@
 import {styled} from 'styled-components';
-import ChangePassword from './ChangePassword';
-import ChangeEmail from './ChangeEmail';
+import ChangePassword from './ChangePasswordForm';
+import ChangeEmail from './ChangeEmailForm';
 import {useContext, useEffect, useState} from 'react';
 import {AccountContext} from '../../services/Account';
 import {useNavigate} from 'react-router-dom';
+import {breakPoints} from '../../utils/layout/breakpoints';
+import {getFontSize} from '../../utils/layout/getFontSize';
 
-const ProfileWrapper = styled.div``;
-const LogoutButtonWrapper = styled.div``;
+const ProfileWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const StyledLogoutButton = styled.div`
+    margin-top: 40px;
+    border: 0;
+    outline: 0;
+    color: black;
+    border-radius: 5em;
+    background-color: lightgrey;
+    &:hover {
+        cursor: pointer;
+        border: 1px solid #9b9b9b;
+    }
+    @media screen and ${breakPoints.mobile} {
+        font-size: ${getFontSize(6)};
+        width: 300px;
+        min-height: 100px;
+    }
+
+    @media screen and ${breakPoints.tabletBig} {
+        font-size: ${getFontSize(5)};
+        width: 200px;
+        min-height: 60px;
+    }
+`;
 
 const ProfilePage = () => {
     const [status, setStatus] = useState<boolean | null>(null);
@@ -37,15 +66,11 @@ const ProfilePage = () => {
     }
 
     return (
-        <ProfileWrapper>
-            <>
-                <h2>Settings</h2>
-                <ChangePassword></ChangePassword>
-                <ChangeEmail></ChangeEmail>
-                <LogoutButtonWrapper>
-                    <button onClick={logout}>Logout</button>
-                </LogoutButtonWrapper>
-            </>
+        <ProfileWrapper data-test-id='ProfileWrapper'>
+            <h2>Profile Settings</h2>
+            <ChangePassword></ChangePassword>
+            <ChangeEmail></ChangeEmail>
+            <StyledLogoutButton onClick={logout}>Logout</StyledLogoutButton>
         </ProfileWrapper>
     );
 };
