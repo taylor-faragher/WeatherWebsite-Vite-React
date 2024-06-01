1fccbbbc46f0cd54a5350687f6f06ac4ce853b40
Taylor Faragher <faragher6@gmail.com>
2024-05-31 19:36:13 -0500

    Removes protected route from router

--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,8 @@ import {Routes, Route, Navigate} from 'react-router-dom';
 import {ErrorPage, HomePage, ResultPage, SignUpPage, ProfilePage} from './pages';
 import './AppRoutes.css';
 import LoginPage from './pages/login/LoginPage';
-import PrivateRoute from './PrivateRoute';
-import useLoginStatus from './hooks/useLoginStatus';
 
 const AppRoutes = () => {
-    const status = useLoginStatus();
     return (
         <Routes>
             <Route path='/' element={<HomePage />} />
@@ -15,14 +12,7 @@ const AppRoutes = () => {
             <Route path='info' element={<Navigate to='/main' replace />} />
             <Route path='signup' element={<SignUpPage />} />
             <Route path='login' element={<LoginPage />} />
-            <Route
-                path='profile'
-                element={
-                    <PrivateRoute isAuthenticated={status}>
-                        <ProfilePage />
-                    </PrivateRoute>
-                }
-            />
+            <Route path='profile' element={<ProfilePage />} />
             <Route path='error' element={<ErrorPage />} />
             <Route path='*' element={<HomePage />} />
         </Routes>
