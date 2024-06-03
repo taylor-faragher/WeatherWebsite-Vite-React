0fe713209ff357e94811a53a33e48d9345c0006c
Taylor Faragher <faragher6@gmail.com>
2024-06-02 22:03:05 -0500

    Adds styling and error handling for sign up page

--- src/pages/signup/SignUpPage.tsx
+++ src/pages/signup/SignUpPage.tsx
@@ -1,30 +1,57 @@
 import {useState} from 'react';
 import {styled} from 'styled-components';
 import UserPool from '../../UserPool';
+import {breakPoints} from '../../utils/layout/breakpoints';
+import {getFontSize} from '../../utils/layout/getFontSize';
+import {useNavigate} from 'react-router-dom';
 
 const SignUpPageWrapper = styled.div``;
+const StyledTitle = styled.h1``;
 const SignUpPageForm = styled.form``;
 const SignUpText = styled.h3``;
 const SignUpEmailInput = styled.input``;
 const SignUpPasswordInput = styled.input``;
-const SignUpButton = styled.button``;
+const SignUpButtonWrapper = styled.div`
+    padding-top: 25px;
+`;
+const SignUpButton = styled.button`
+    width: 100px;
+    height: 30px;
+`;
+const StyledErrorMessage = styled.div`
+    color: red;
+    @media screen and ${breakPoints.mobile} {
+        font-size: ${getFontSize(4)};
+    }
+    @media screen and ${breakPoints.tabletBig} {
+        font-size: ${getFontSize(4)};
+        margin: 20px 0 0 0;
+    }
+`;
 
 const SignUpPage = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [displayError, setDisplayError] = useState(false);
+    const [errorText, setErrorText] = useState('');
+    const navigate = useNavigate();
 
     const onSubmit = event => {
         event.preventDefault();
         UserPool.signUp(email, password, [], [], (err, data) => {
             if (err) {
                 console.log('Signup error: ', err);
+                setErrorText(err.message);
+                setDisplayError(true);
             }
+            navigate('/login');
             console.log(data);
         });
     };
 
     return (
         <SignUpPageWrapper data-test-id='SignUpPageWrapper'>
+            <StyledTitle>Sign Up</StyledTitle>
             <SignUpPageForm onSubmit={onSubmit} data-test-id='SignUpPageForm'>
                 <SignUpText data-test-id='SignUpTextUsername'>Username</SignUpText>
                 <SignUpEmailInput
@@ -38,9 +65,16 @@ const SignUpPage = () => {
                     onChange={event => setPassword(event.target.value)}
                     data-test-id='SignUpPasswordInput'
                 ></SignUpPasswordInput>
-                <SignUpButton type='submit' data-test-id='SignUpButton'>
-                    Signup
-                </SignUpButton>
+                <SignUpButtonWrapper>
+                    <SignUpButton type='submit' data-test-id='SignUpButton'>
+                        Signup
+                    </SignUpButton>
+                </SignUpButtonWrapper>
+                {displayError && (
+                    <div>
+                        <StyledErrorMessage data-test-id='ErrorMessage'>{errorText}</StyledErrorMessage>
+                    </div>
+                )}
             </SignUpPageForm>
         </SignUpPageWrapper>
     );
