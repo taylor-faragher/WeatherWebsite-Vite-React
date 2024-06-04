72e256979f2565a7de6f320a51c37e35e7096979
Taylor Faragher <faragher6@gmail.com>
2024-06-04 15:21:47 -0500

    Adds styling and navigation to forgot password form

--- src/pages/shared/ForgotPasswordForm.tsx
+++ src/pages/shared/ForgotPasswordForm.tsx
@@ -1,15 +1,64 @@
 import {useState} from 'react';
 import UserPool from '../../UserPool';
 import {CognitoUser} from 'amazon-cognito-identity-js';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import {styled} from 'styled-components';
+import {getFontWeight} from '../../utils/layout/getFontWeight';
+import {breakPoints} from '../../utils/layout/breakpoints';
+import {getFontSize} from '../../utils/layout/getFontSize';
+
+const ForgotPasswordPageForm = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const LinkDiv = styled.div`
+    padding-top: 25px;
+    font-weight: ${getFontWeight('heavy')};
+`;
+
+const StyledInput = styled.input`
+    @media screen and ${breakPoints.mobile} {
+        font-size: ${getFontSize(4)};
+        margin-bottom: 25px;
+        margin-top: 15px;
+        max-width: 350px;
+    }
+    @media screen and ${breakPoints.tabletBig} {
+        font-size: ${getFontSize(4)};
+        margin-bottom: 25px;
+    }
+`;
+
+const StyledVerificationButton = styled.button`
+    @media screen and ${breakPoints.mobile} {
+        font-size: ${getFontSize(6)};
+        width: 200px;
+        height: 60px;
+    }
+    @media screen and ${breakPoints.tablet} {
+        font-size: ${getFontSize(4)};
+        width: 300px;
+        height: 50px;
+    }
+`;
 
 const ForgotPasswordForm = ({loginSwitch}) => {
     const [email, setEmail] = useState<string>('');
     const [verificationCode, setVerificationCode] = useState<string>('');
     const [newPassword, setNewPassword] = useState<string>('');
     const [step, setStep] = useState<number>(1);
+    const navigate = useNavigate();
 
-    const handleForgotPassword = () => {
+    const handleForgotPassword = event => {
+        event.preventDefault();
         const userData = {
             Username: email,
             Pool: UserPool,
@@ -28,7 +77,8 @@ const ForgotPasswordForm = ({loginSwitch}) => {
         });
     };
 
-    const handleResetPassword = () => {
+    const handleResetPassword = event => {
+        event.preventDefault();
         const userData = {
             Username: email,
             Pool: UserPool,
@@ -47,51 +97,64 @@ const ForgotPasswordForm = ({loginSwitch}) => {
         });
     };
 
+    const handleLoginNav = () => {
+        navigate(0);
+    };
+
     return (
         <>
-            <div>
+            <ForgotPasswordPageForm data-test-id='ForgotPasswordPageForm'>
                 {step === 1 && (
-                    <div>
-                        <input
+                    <StyledForm onSubmit={e => handleForgotPassword(e)} data-test-id='VerificationCodeInputWrapper'>
+                        <StyledInput
                             type='email'
                             value={email}
                             onChange={e => setEmail(e.target.value)}
                             placeholder='Email'
                         />
-                        <button onClick={handleForgotPassword}>Send Verification Code</button>
-                    </div>
+                        <StyledVerificationButton data-test-id='SendVerificationCodeButton' type='submit'>
+                            Send Verification Code
+                        </StyledVerificationButton>
+                    </StyledForm>
                 )}
                 {step === 2 && (
                     <div>
-                        <h2>Reset Password</h2>
-                        <input
-                            type='text'
-                            value={verificationCode}
-                            onChange={e => setVerificationCode(e.target.value)}
-                            placeholder='Verification Code'
-                        />
-                        <input
-                            type='password'
-                            value={newPassword}
-                            onChange={e => setNewPassword(e.target.value)}
-                            placeholder='New Password'
-                        />
-                        <button onClick={handleResetPassword}>Confirm New Password</button>
+                        <h2>Please Check Email for Verification Code</h2>
+                        <StyledForm onSubmit={e => handleResetPassword(e)}>
+                            <StyledInput
+                                type='text'
+                                value={verificationCode}
+                                onChange={e => setVerificationCode(e.target.value)}
+                                placeholder='Verification Code'
+                            />
+                            <StyledInput
+                                type='password'
+                                value={newPassword}
+                                onChange={e => setNewPassword(e.target.value)}
+                                placeholder='New Password'
+                            />
+                            <StyledVerificationButton onClick={handleResetPassword}>
+                                Confirm New Password
+                            </StyledVerificationButton>
+                        </StyledForm>
                     </div>
                 )}
                 {step === 3 && (
                     <div>
                         <h2>Password Reset Successful</h2>
                         <p>You can now log in with your new password.</p>
+                        <button onClick={handleLoginNav}>Login</button>
                     </div>
                 )}
-            </div>
-            <div>
-                Remebered your login?{' '}
-                <Link to='#' onClick={loginSwitch}>
-                    Login Here!
-                </Link>
-            </div>
+            </ForgotPasswordPageForm>
+            {step !== 3 && (
+                <LinkDiv>
+                    Remebered your login?{' '}
+                    <Link to='#' onClick={loginSwitch}>
+                        Login Here!
+                    </Link>
+                </LinkDiv>
+            )}
         </>
     );
 };
