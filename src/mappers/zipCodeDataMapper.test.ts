60d4e41e03530b744c75e6081ebcfa1f83f4493c
Taylor Faragher <faragher6@gmail.com>
2024-06-10 00:56:11 -0500

    Updates unit tests

--- src/mappers/zipCodeDataMapper.test.ts
+++ src/mappers/zipCodeDataMapper.test.ts
@@ -21,15 +21,15 @@ describe('mapZipCodeData', () => {
         (pascalize as jest.Mock).mockReturnValue(mockPascalizedDescription);
 
         const expectedResult: WeatherItem = {
-            currentTemp: 59.76,
-            windSpeed: 5.75,
+            currentTemp: 59,
+            windSpeed: 10,
             feelsLike: 59,
             humidity: 100,
             windDirection: 360,
             description: mockPascalizedDescription,
-            majorCity: 'TownUSA',
+            majorCity: 'Smyrna',
             image: {
-                image: '04d.svg',
+                image: 'i2d.svg',
                 imageAltText: mockAltText,
             },
         };
@@ -37,7 +37,7 @@ describe('mapZipCodeData', () => {
         const result = await mapZipCodeData(mockData);
 
         expect(getPicAltText).toHaveBeenCalledWith('i2d');
-        expect(pascalize).toHaveBeenCalledWith('overcast clouds');
+        expect(pascalize).toHaveBeenCalledWith('windy');
         expect(result).toEqual(expectedResult);
     });
 });
