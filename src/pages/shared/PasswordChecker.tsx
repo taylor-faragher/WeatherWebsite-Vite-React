5cbe1e067f1c746f34cb88d405cb142747c85a6e
Taylor Faragher <faragher6@gmail.com>
2024-06-15 19:32:46 -0500

    Adds password checker component

---
+++ src/pages/shared/PasswordChecker.tsx
@@ -0,0 +1,79 @@
+import {CharacterType} from 'src/types/types';
+import {styled} from 'styled-components';
+
+const PasswordCheckerWrapper = styled.div`
+    text-align: left;
+`;
+
+const PasswordUnorderedList = styled.ul``;
+
+const StyledPasswordLengthCheck = styled.li<{characterType: CharacterType}>`
+    list-style-type: ${({characterType}) => `"${getUnicodeCharacter(characterType)}"`};
+    color: ${({characterType}) => `${getTextColor(characterType)}`};
+    padding-left: 5px;
+`;
+
+const StyledUpperCaseCheck = styled.li<{characterType: CharacterType}>`
+    list-style-type: ${({characterType}) => `"${getUnicodeCharacter(characterType)}"`};
+    color: ${({characterType}) => `${getTextColor(characterType)}`};
+    padding-left: 5px;
+`;
+
+const StyledNumberCheck = styled.li<{characterType: CharacterType}>`
+    list-style-type: ${({characterType}) => `"${getUnicodeCharacter(characterType)}"`};
+    color: ${({characterType}) => `${getTextColor(characterType)}`};
+    padding-left: 5px;
+`;
+
+const StyledSpecialCharacterCheck = styled.li<{characterType: CharacterType}>`
+    list-style-type: ${({characterType}) => `"${getUnicodeCharacter(characterType)}"`};
+    color: ${({characterType}) => `${getTextColor(characterType)}`};
+    padding-left: 5px;
+`;
+
+const unicodeMap = {
+    none: '\u2022',
+    correct: '\u2705',
+    incorrect: '\u0078',
+};
+
+const textColorMap = {
+    none: 'inherit',
+    correct: 'green',
+    incorrect: 'red',
+};
+
+const getUnicodeCharacter = value => {
+    return unicodeMap[value];
+};
+
+const getTextColor = value => {
+    return textColorMap[value];
+};
+
+type PasswordCheckerProps = {
+    lengthType: CharacterType;
+    upperCaseType: CharacterType;
+    numberType: CharacterType;
+    specialCharacterType: CharacterType;
+};
+
+const PasswordChecker = (props: PasswordCheckerProps) => {
+    return (
+        <PasswordCheckerWrapper data-test-id='PasswordCheckerWrapper'>
+            <p>Password must contain: </p>
+            <PasswordUnorderedList>
+                <StyledPasswordLengthCheck characterType={props.lengthType}>
+                    Eight or more Characters
+                </StyledPasswordLengthCheck>
+                <StyledUpperCaseCheck characterType={props.upperCaseType}>One Uppercase Letter</StyledUpperCaseCheck>
+                <StyledNumberCheck characterType={props.numberType}>A number</StyledNumberCheck>
+                <StyledSpecialCharacterCheck characterType={props.specialCharacterType}>
+                    A Special Character
+                </StyledSpecialCharacterCheck>
+            </PasswordUnorderedList>
+        </PasswordCheckerWrapper>
+    );
+};
+
+export default PasswordChecker;
