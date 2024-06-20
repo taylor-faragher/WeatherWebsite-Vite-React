932511b6eccdf309efd44b42188c60bf81f04b27
Taylor Faragher <faragher6@gmail.com>
2024-06-20 14:03:30 -0500

    Updates unit tests for zipCodeDataMapper for new capitalize method and added values]

--- src/mappers/zipCodeDataMapper.test.ts
+++ src/mappers/zipCodeDataMapper.test.ts
@@ -1,5 +1,5 @@
-import {pascalize} from '../utils/pascalize';
-import {WeatherItem} from '../types/types';
+import {capitalizeWords} from '../utils/capitalizeWords';
+import {HourlyData, WeatherItem} from '../types/types';
 import {mockData} from '../utils/testData/testData';
 import {getPicAltText} from './getPicAltText';
 import {mapZipCodeData} from './zipCodeDataMapper';
@@ -8,34 +8,44 @@ jest.mock('./getPicAltText', () => ({
     getPicAltText: jest.fn(),
 }));
 
-jest.mock('../utils/pascalize', () => ({
-    pascalize: jest.fn(),
+jest.mock('../utils/capitalizeWords', () => ({
+    capitalizeWords: jest.fn(),
 }));
 
 describe('mapZipCodeData', () => {
     test('should return mapped WeatherItem when data is provided', async () => {
         const mockAltText = 'Rainy weather';
-        const mockPascalizedDescription = 'Light Rain';
+        const mockCapitalizedDescription = 'Light Rain';
+        const mockHourlyData: HourlyData[] = [
+            {
+                time: '2:00 PM',
+                temp: 88,
+                icon: '01d',
+                description: 'clear sky',
+            },
+        ];
 
         (getPicAltText as jest.Mock).mockResolvedValue(mockAltText);
-        (pascalize as jest.Mock).mockReturnValue(mockPascalizedDescription);
+        (capitalizeWords as jest.Mock).mockReturnValue(mockCapitalizedDescription);
 
         const expectedResult: WeatherItem = {
             currentTemp: 88,
             windSpeed: 10.36,
             humidity: 63,
             windDirection: 130,
-            description: mockPascalizedDescription,
+            description: mockCapitalizedDescription,
+            hourlyData: mockHourlyData,
             image: {
                 image: '01d.svg',
                 imageAltText: mockAltText,
             },
+            majorCity: 'SomeCity',
         };
 
         const result = await mapZipCodeData(mockData);
 
         expect(getPicAltText).toHaveBeenCalledWith('01d');
-        expect(pascalize).toHaveBeenCalledWith('clear sky');
+        expect(capitalizeWords).toHaveBeenCalledWith('clear sky');
         expect(result).toEqual(expectedResult);
     });
 });
