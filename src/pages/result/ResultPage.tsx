4183b1b4b120e65360ac5748e9f06c220e614ddf
Taylor Faragher <faragher6@gmail.com>
2024-06-20 14:05:06 -0500

    Updates results page to include new result components and logic to switch between them based on user

--- src/pages/result/ResultPage.tsx
+++ src/pages/result/ResultPage.tsx
@@ -1,12 +1,16 @@
 import {useNavigate, useSearchParams} from 'react-router-dom';
 import styled from 'styled-components';
-import {useState} from 'react';
+import {useContext, useState} from 'react';
 import {Loader} from '../shared/Loader';
 import {breakPoints} from '../../utils/layout/breakpoints';
 import {getFontSize} from '../../utils/layout/getFontSize';
 import {ZipCodeForm} from '../shared/ZipCodeForm';
 import {fetchWeather} from '../../services/zipCode/WeatherGateway';
 import {useQuery} from '@tanstack/react-query';
+import PremiumResults from './PremiumResults';
+import {WeatherItem} from 'src/types/types';
+import FreemiumResults from './FreemiumResults';
+import {AccountContext} from '../../services/Account';
 
 const ResultsSection = styled.div`
     display: flex;
@@ -37,72 +41,6 @@ const ZipCodeTitle = styled.h1`
     }
 `;
 
-const WeatherResultsSection = styled.div`
-    display: flex;
-    flex-direction: column;
-    text-align: center;
-    @media screen and ${breakPoints.mobile} {
-        font-size: ${getFontSize(7)};
-    }
-
-    @media screen and ${breakPoints.tablet} {
-        font-size: ${getFontSize(9)};
-    }
-`;
-
-const WeatherPicWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    @media screen and ${breakPoints.mobile} {
-        width: 100%;
-    }
-    @media screen and ${breakPoints.tablet} {
-        width: 100%;
-`;
-
-const WeatherPic = styled.img`
-    width: 100%;
-    height: auto;
-`;
-
-const TempWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    @media screen and ${breakPoints.mobile} {
-        font-size: ${getFontSize(10)};
-    }
-    @media screen and ${breakPoints.tablet} {
-        font-size: ${getFontSize(11)};
-        margin-bottom: -40px;
-    }
-`;
-
-const ThermoPicture = styled.img`
-    @media screen and ${breakPoints.mobile} {
-        margin-top: -20px;
-        margin-left: -80px;
-        margin-right: -50px;
-        height: 250px;
-    }
-    @media screen and ${breakPoints.tablet} {
-        margin-top: -40px;
-        margin-top: 0px;
-        margin-left: -50px;
-        height: 200px;
-    }
-`;
-
-const StyledDescription = styled.div`
-    width: 100%;
-    @media screen and ${breakPoints.mobile} {
-        font-size: ${getFontSize(7)};
-    }
-    @media screen and ${breakPoints.tablet} {
-        font-size: ${getFontSize(10)};
-    }
-`;
-
 const ResultPage = () => {
     const [searchParams] = useSearchParams();
     const [zipCode, setZipCode] = useState(searchParams.get('zipCode'));
@@ -111,10 +49,7 @@ const ResultPage = () => {
         queryKey: ['fetchWeather', zipCode],
         queryFn: () => fetchWeather(zipCode as string),
     });
-    const currentTemp = Math.round(data?.currentTemp as number);
-    const description = data?.description;
-    const weatherPic = `/assets/${data?.image.image}`;
-    const thermoPic = `/assets/thermometer-fahrenheit.svg`;
+    const {user} = useContext(AccountContext);
 
     if (error) navigate('/error');
 
@@ -129,23 +64,9 @@ const ResultPage = () => {
                 )}
                 {!isLoading && (
                     <>
-                        <WeatherResultsSection data-test-id='ResultsPage_WeatherResultsSection'>
-                            <WeatherPicWrapper data-test-id='ResultPage_WeatherPicWrapper'>
-                                <WeatherPic
-                                    src={weatherPic}
-                                    alt={data?.image.imageAltText}
-                                    data-test-id='ResultPage_WeatherPic'
-                                ></WeatherPic>
-                            </WeatherPicWrapper>
-                            <div data-test-id='ResultPage_WeatherInfoWrapper'>
-                                <TempWrapper data-test-id='ResultPage_TempWrapper'>
-                                    {currentTemp} <ThermoPicture src={thermoPic}></ThermoPicture>
-                                </TempWrapper>
-                                <StyledDescription data-test-id='ResultPage_Description'>
-                                    {description}
-                                </StyledDescription>
-                            </div>
-                        </WeatherResultsSection>
+                        {user && <PremiumResults data={data as WeatherItem} />}
+                        {!user && <FreemiumResults data={data as WeatherItem} />}
+
                         <ZipCodeForm
                             data-test-id='ResultPage_ZipCodeForm'
                             placeHolderText={'Search Again!'}
