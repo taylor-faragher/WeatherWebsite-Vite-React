6241fa2ae82ceed68ecb98fde33f05fa9dd73ea8
Taylor Faragher <faragher6@gmail.com>
2024-06-20 14:03:09 -0500

    Adds majorcity and new mapper name to zipCodeDataMapper

--- src/mappers/zipCodeDataMapper.ts
+++ src/mappers/zipCodeDataMapper.ts
@@ -1,11 +1,12 @@
 import {WeatherItem, WeatherResults} from '../types/types';
-import {pascalize} from '../utils/pascalize';
+import {capitalizeWords} from '../utils/capitalizeWords';
 import {getPicAltText} from './getPicAltText';
 
 export const mapZipCodeData = async (data: WeatherResults): Promise<WeatherItem> => {
     const picName = data.current.icon;
     const weatherPic = {image: `${picName}.svg`, imageAltText: await getPicAltText(picName)};
-    const description = await pascalize(data.current.description);
+    const description = await capitalizeWords(data.current.description);
+    const hourlyData = data.hourly;
 
     return {
         currentTemp: data?.current.temperature,
@@ -13,6 +14,8 @@ export const mapZipCodeData = async (data: WeatherResults): Promise<WeatherItem>
         windSpeed: data?.current.windSpeed,
         windDirection: data?.current.windDirection,
         description: description,
+        hourlyData: hourlyData,
         image: weatherPic,
+        majorCity: data.area.majorCity,
     };
 };
