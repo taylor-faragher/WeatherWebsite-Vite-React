3cd995bcc165fce24ca1416f15e0bca48f3664ac
Taylor Faragher <faragher6@gmail.com>
2024-06-29 14:44:30 -0500

    Adds new premium gateway

---
+++ src/gateways/PremiumWeatherGateway.ts
@@ -0,0 +1,34 @@
+import {PremiumWeatherItem} from 'src/types/types';
+import {premiumDataMapper} from '../mappers/premiumDataMapper';
+
+export const fetchPremiumWeather = async (zipCode: string, user): Promise<PremiumWeatherItem> => {
+    if (!zipCode) {
+        return Promise.reject(new Error('Bad Request'));
+    }
+    try {
+        const keys = Object.keys(localStorage);
+        const keyStorage = keys.find(key => key.includes(`${user.username}.accessToken`));
+        const tokenKey = keyStorage ? localStorage.getItem(keyStorage) : null;
+
+        const headers = {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            'x-api-key': 'uFaywcvpQj94HeeNC8ESd3w5eSS8NcxKaILjvkVq',
+            authorization: `Bearer ${tokenKey}`,
+        };
+
+        const response: Response = await fetch(`https://dev.taylorsweatherapi.com/premium/?zipcode=${zipCode}`, {
+            headers: headers,
+        });
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(JSON.stringify(data.message));
+        }
+        const mappedData = await premiumDataMapper(data);
+
+        return mappedData;
+    } catch {
+        throw new Error('Error getting Premium Data');
+    }
+};
