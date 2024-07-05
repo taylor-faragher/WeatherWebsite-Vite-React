00f994b5de9dc134804caab12b3b309c24aa1e3c
Taylor Faragher <faragher6@gmail.com>
2024-07-05 12:53:40 -0500

    Adds new hook for getting user session and then triggers appropriate api call based on if user is present or not

--- src/pages/result/ResultPage.tsx
+++ src/pages/result/ResultPage.tsx
@@ -1,6 +1,6 @@
 import {useNavigate, useSearchParams} from 'react-router-dom';
 import styled from 'styled-components';
-import {useContext, useState} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import {Loader} from '../shared/Loader';
 import {breakPoints} from '../../utils/layout/breakpoints';
 import {getFontSize} from '../../utils/layout/getFontSize';
@@ -9,8 +9,10 @@ import PremiumResults from './PremiumResults';
 import {FreemiumWeatherItem, PremiumWeatherItem} from 'src/types/types';
 import FreemiumResults from './FreemiumResults';
 import {useScrollToTop} from '../../hooks/useScrollToTop';
-import useWeather from '../../services/getWeather';
 import {AccountContext} from '../../services/Account';
+import {fetchPremiumWeather} from '../../gateways/PremiumWeatherGateway';
+import {useQuery} from '@tanstack/react-query';
+import {fetchFreemiumWeather} from '../../gateways/FreemiumWeatherGateway';
 
 const ResultsSection = styled.div`
     display: flex;
@@ -44,11 +46,48 @@ const ZipCodeTitle = styled.h1`
 const ResultPage = () => {
     const [searchParams] = useSearchParams();
     const navigate = useNavigate();
+    const [showPremium, setShowPremium] = useState(false);
+    const [showFreemium, setShowFreemium] = useState(false);
+    const [user, setUser] = useState(null);
     const [zipCode, setZipCode] = useState(searchParams.get('zipCode'));
-    const {user} = useContext(AccountContext);
-    const {data, error, isLoading} = useWeather(zipCode as string);
+    const {getSession} = useContext(AccountContext);
 
-    if (error) navigate('/error');
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const {user} = await getSession();
+                if (user) {
+                    setUser(user);
+                    setShowPremium(true);
+                }
+            } catch {
+                setShowFreemium(true);
+            }
+        };
+        fetchUser();
+    }, [getSession]);
+
+    const {
+        data: premiumData,
+        error,
+        isLoading,
+    } = useQuery({
+        queryKey: ['fetchPremiumWeather', zipCode],
+        queryFn: () => fetchPremiumWeather(zipCode as string, user),
+        enabled: showPremium,
+    });
+
+    const {
+        data: freemiumData,
+        error: freemiumError,
+        isLoading: freemiumIsLoading,
+    } = useQuery({
+        queryKey: ['fetchFreemiumWeather', zipCode],
+        queryFn: () => fetchFreemiumWeather(zipCode as string),
+        enabled: showFreemium,
+    });
+
+    if (error || freemiumError) navigate('/error');
 
     useScrollToTop();
 
@@ -61,18 +100,16 @@ const ResultPage = () => {
                         <Loader />
                     </div>
                 )}
-                {!isLoading && (
-                    <>
-                        {user && <PremiumResults data={data as PremiumWeatherItem} />}
-                        {!user && <FreemiumResults data={data as FreemiumWeatherItem} />}
-
-                        <ZipCodeForm
-                            data-test-id='ResultPage_ZipCodeForm'
-                            placeHolderText={'Search Again!'}
-                            setZipCode={setZipCode}
-                        />
-                    </>
+                {!freemiumIsLoading && (
+                    <>{showFreemium && <FreemiumResults data={freemiumData as unknown as FreemiumWeatherItem} />}</>
                 )}
+
+                {!isLoading && <>{showPremium && <PremiumResults data={premiumData as PremiumWeatherItem} />}</>}
+                <ZipCodeForm
+                    data-test-id='ResultPage_ZipCodeForm'
+                    placeHolderText={'Search Again!'}
+                    setZipCode={setZipCode}
+                />
             </ResultsSection>
         </>
     );
