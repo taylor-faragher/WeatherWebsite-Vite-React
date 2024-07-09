af4994ead991a0b5a76f463e12259afc0671cb82
Taylor Faragher <faragher6@gmail.com>
2024-07-09 16:46:54 -0500

    Adds notification provider and notification component to app layer. Both are needed for displaying notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import styled, {ThemeProvider} from 'styled-components';
 import AppRoutes from './router';
 import {GlobalStyles} from './GlobalStyles';
-import {Account} from './services/Account';
+import {AccountProvider} from './services/AccountProvider';
 import {darkTheme, lightTheme} from './Theme';
 import {Header} from './pages/shared/Header/Header';
 import {useDarkMode} from './hooks/useDarkMode';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {NotificationProvider} from './services/NotificationProvider';
+import Notification from './pages/shared/Notifications/Notification';
 
 const AppWrapper = styled.div`
     width: 100vw;
@@ -16,17 +18,22 @@ const queryClient = new QueryClient();
 export const App = () => {
     const [theme, themeToggler, mountedComponent] = useDarkMode();
     const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
     if (!mountedComponent) return <div />;
+
     return (
         <ThemeProvider theme={themeMode}>
             <QueryClientProvider client={queryClient}>
                 <GlobalStyles />
-                <Account>
-                    <AppWrapper data-test-id='AppWrapper'>
-                        <Header data-test-id='Header' theme={theme} themeSwitch={themeToggler}></Header>
-                        <AppRoutes />
-                    </AppWrapper>
-                </Account>
+                <NotificationProvider>
+                    <AccountProvider>
+                        <AppWrapper data-test-id='AppWrapper'>
+                            <Header data-test-id='Header' theme={theme} themeSwitch={themeToggler}></Header>
+                            <Notification></Notification>
+                            <AppRoutes />
+                        </AppWrapper>
+                    </AccountProvider>
+                </NotificationProvider>
             </QueryClientProvider>
         </ThemeProvider>
     );
