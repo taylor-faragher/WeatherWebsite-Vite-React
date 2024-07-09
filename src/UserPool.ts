72b79c22ebca8f8178c693df604906ea81f30f1f
Taylor Faragher <faragher6@gmail.com>
2024-07-09 16:45:41 -0500

    Removes old files

--- src/UserPool.ts
+++
@@ -1,7 +0,0 @@
-import {CognitoUserPool} from 'amazon-cognito-identity-js';
-
-const poolData = {
-    UserPoolId: 'us-east-1_dbPG7AUx8',
-    ClientId: '2uu0pjvrs5l12tljij3gagduca',
-};
-export default new CognitoUserPool(poolData);
