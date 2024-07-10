040d2f0ba1e7ff65dc3015609128c10dd9bb4c53
Taylor Faragher <faragher6@gmail.com>
2024-07-10 12:45:50 -0500

    Updates styling of notification for mobile and desktop

--- src/pages/shared/Notifications/Notification.tsx
+++ src/pages/shared/Notifications/Notification.tsx
@@ -3,30 +3,38 @@ import {NotificationContext} from '../../../services/NotificationProvider';
 import {styled} from 'styled-components';
 import {getFontSize} from '../../../utils/layout/getFontSize';
 import {getFontWeight} from '../../../utils/layout/getFontWeight';
+import {breakPoints} from '../../../utils/layout/breakpoints';
 
 const Alert = styled.div<{$toBeDisplayed; $notificationType}>`
     display: ${({$toBeDisplayed}) => ($toBeDisplayed ? 'inline' : 'none')};
     position: absolute;
-    font-size: ${getFontSize(4)};
+    font-size: ${getFontSize(3)};
     left: 50%;
-    top: 15%;
     border-radius: 10px;
     transform: translate(-50%, 0);
     z-index: 10;
     text-align: center;
     background-color: ${({$notificationType}) => `${getNotificationBackgroundColor($notificationType)}`};
-    color: white;
+    color: ${({$notificationType, theme}) => theme.notificationTextColor[$notificationType]};
     margin-bottom: 15px;
     min-width: 200px;
+    text-wrap: balance;
+    @media screen and ${breakPoints.mobile} {
+        top: 3%;
+        width: 90%;
+        padding-right: 25px;
+    }
+    @media screen and ${breakPoints.tabletBig} {
+        top: 10%;
+        width: 40%;
+        padding-right: 35px;
+    }
 `;
 
 const AlertClose = styled.span`
     position: absolute;
-    right: 5%;
-    top: 20%;
-    color: white;
     font-weight: ${getFontWeight('bold')};
-    font-size: ${getFontSize(4)};
+    font-size: ${getFontSize(5)};
     line-height: 20px;
     cursor: pointer;
     transition: 0.3s;
@@ -34,12 +42,19 @@ const AlertClose = styled.span`
     &:hover {
         color: black;
     }
+    @media screen and ${breakPoints.mobile} {
+        right: 3%;
+        top: 10%;
+    }
+    @media screen and ${breakPoints.tabletBig} {
+        right: 2%;
+    }
 `;
 
 const notificationColorMap = {
     positive: 'green',
     negative: 'red',
-    warning: 'yellow',
+    warning: '#ffeb3b',
 };
 
 const getNotificationBackgroundColor = value => {
